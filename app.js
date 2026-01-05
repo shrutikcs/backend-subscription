fc2933f91bf9d78300455134b89e344a0dff2717
shrutikcs <vlaveanticheat@gmail.com>
2026-01-05 17:00:07 +0530

    add ratelimit, bot protection and protected route

--- app.js
+++ app.js
@@ -6,18 +6,20 @@ import subscriptionRouter from "./routes/subscription.route.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(arcjetMiddleware);
 
+app.use(errorMiddleware);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("hello world");
